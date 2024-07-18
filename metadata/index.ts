45789d9f75bbf1aebfc3556ad5fbd2da4ac3099f
朵琳 <83268912+Dolyn157@users.noreply.github.com>
2024-07-18 22:46:07 +0800

    fix the wrong plainTargetDomain

--- metadata/index.ts
+++ metadata/index.ts
@@ -4,12 +4,12 @@ export const siteShortName = '朵琳的博客'
 export const siteDescription = '朵琳的自留地'
 
 /** 文档所在目录 */
-export const include = ['笔记', '生活','艺术评论', '我的画册']
+export const include = ['笔记', '生活','博文', '我的画册']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/Dolyn157/Dolyn-s-MusicReview'
+export const githubRepoLink = 'https://github.com/Dolyn157/Dolyn-Blog-Demo'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'orangeplanet.club'
+export const plainTargetDomain = 'blog.high-spirits.me'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
